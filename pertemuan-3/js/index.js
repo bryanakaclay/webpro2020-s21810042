7bbf1f053737f4d349c387229e802dd3ecc922de
bryanakaclay <js.bryann@gmail.com>
2020-08-25 15:00:48 +0800

    pertemuan-3

---
+++ pertemuan-3/js/index.js
@@ -0,0 +1,30 @@
+console.log("Hello World YAWWW!!!");
+
+const firstName = "John";
+let age = 20;
+age = 21;
+const IsMarried = true;
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof IsMarried);
+console.log(typeof age + " " + IsMarried);
+
+const info = 
+"My Name Is " + firstName + ", I'm " + age + " years old." + IsMarried;
+
+console.log(typeof info);
+
+// for(var i=0; i<10; i++){
+//     console.log(i);
+// }
+// console.log(i);
+
+
+/* 
+    ----- 2. Operator -----
+*/
+let bilangan = 10;
+
+console.log(bilangan == "10");      //loose equality
+console.log(bilangan === "10");     //strict equality
