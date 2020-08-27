da23e7958c7314d39d7e6826b602faa07172ab4b
bryanakaclay <js.bryann@gmail.com>
2020-08-27 14:16:24 +0800

    pertemuan-4

---
+++ pertemuan-4/penjumlahan.js
@@ -0,0 +1,21 @@
+function penjumlahan (a, b) {
+    let luasA = a * a, luasB = b * b, total = luasA + luasB;
+    return total;
+}
+
+console.log(penjumlahan(8, 3));
+
+
+function tambah(a, b){
+    return (a + b);
+}
+function kali(a, b){
+    return (a * b);
+}
+
+console.log(tambah(kali(5, 2), kali(10, 2)));
+
+
+const jumlah (a, b){
+    return (a + b);
+}
