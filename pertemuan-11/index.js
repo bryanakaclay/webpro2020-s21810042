2ce5079d736846705d54bdc8378ce31a58114946
bryanakaclay <js.bryann@gmail.com>
2020-09-29 14:07:14 +0800

    pertemuan-11

--- pertemuan-11/index.js
+++ pertemuan-11/index.js
@@ -13,3 +13,17 @@ const hitungan = (bil1, bil2) =>{
     return result;
 }
 console.log(hitungan(5, 5));
+
+/* ==||==   EXERCISE   ==||== */
+//Convert ke ES6
+
+const yearUntilRetirement = (year = 2020, firstName = "Ryan") => {
+    let age = 28;
+    let retirement = 65 - age;
+    if(retirement > 0){
+        console.log(`${firstName} retired in ${retirement} years.`);
+    } else {
+        console.log(`${firstName} is already retired.`);
+    }
+}
+yearUntilRetirement();
