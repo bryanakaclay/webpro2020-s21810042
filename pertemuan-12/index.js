dd4bc251a07514a833bff1d81b5049a86e7ca276
bryanakaclay <js.bryann@gmail.com>
2020-10-01 14:02:53 +0800

    pertemuan-12

--- pertemuan-12/index.js
+++ pertemuan-12/index.js
@@ -2,37 +2,37 @@
 
 //--- Single Argument
 const Age = (year) => {
-    return 2020 - year;
-};
-console.log(Age(1991));
+    return 2020 - year
+}
+console.log(Age(1991))
 
 // --- Multi Argument
 const Ages = (birth, current) => {
-    return current - birth;
-};
-console.log(Ages(1992, 2020));
+    return current - birth
+}
+console.log(Ages(1992, 2020))
 
 // --- No Argument
 const Agess = () => {
-    return 2020 - 1993;
-};
-console.log(Agess());
+    return 2020 - 1993
+}
+console.log(Agess())
 
 // --- No Argument with Implicit Return
-const Agesss = () => 2020 - 1994;
-console.log(Agesss());
+const Agesss = () => 2020 - 1994
+console.log(Agesss())
 
 // --- 
-const years = [1990, 1992, 2000, 2005];
+const years = [1990, 1992, 2000, 2005]
 const calc = years.map(function(fun){
-    return 2020 - fun;
-});
-const calcc = years.map((fun) => 2020 - fun);
+    return 2020 - fun
+})
+const calcc = years.map((fun) => 2020 - fun)
 
-console.log(calcc);
+console.log(calcc)
 
 
 /* ///----- DEFAULT PARAMETER -----\\\ */
 
-const hitung = (ultah, sekarang = 2020) => sekarang - ultah;
-console.log(hitung(1991));
+const hitung = (ultah, sekarang = 2020) => sekarang - ultah
+console.log(hitung(1991))
