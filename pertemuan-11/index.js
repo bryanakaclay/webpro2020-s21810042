c370d7c1ffa5d87174ebb9cdfe6081acd1b2029f
bryanakaclay <js.bryann@gmail.com>
2020-10-01 14:02:47 +0800

    pertemuan-11

--- pertemuan-11/index.js
+++ pertemuan-11/index.js
@@ -2,28 +2,28 @@
 
 //--- tanpa parameter ---
 const namabuah = () => {
-    const buah = ["Anggur", "Apel", "Jeruk", "Durian", "Nanas", "Semangka", "Kokoa"];
-    return buah[Math.floor(Math.random()*(buah.length))];
+    const buah = ["Anggur", "Apel", "Jeruk", "Durian", "Nanas", "Semangka", "Kokoa"]
+    return buah[Math.floor(Math.random()*(buah.length))]
 }
-console.log(namabuah());
+console.log(namabuah())
 
 //--- dengan parameter ---
 const hitungan = (bil1, bil2) =>{
-    const result = bil1 + bil2;
-    return result;
+    const result = bil1 + bil2
+    return result
 }
-console.log(hitungan(5, 5));
+console.log(hitungan(5, 5))
 
 /* ==||==   EXERCISE   ==||== */
 //Convert ke ES6
 
 const yearUntilRetirement = (year = 2020, firstName = "Ryan") => {
-    let age = 28;
-    let retirement = 65 - age;
+    let age = 28
+    let retirement = 65 - age
     if(retirement > 0){
-        console.log(`${firstName} retired in ${retirement} years.`);
+        console.log(`${firstName} retired in ${retirement} years.`)
     } else {
-        console.log(`${firstName} is already retired.`);
+        console.log(`${firstName} is already retired.`)
     }
 }
-yearUntilRetirement();
+yearUntilRetirement()
