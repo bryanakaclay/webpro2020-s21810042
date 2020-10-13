66c2256dc4b55b221889b7b9dfcc6ec3f1b3a748
bryanakaclay <js.bryann@gmail.com>
2020-10-13 13:45:44 +0800

    pertemuan-16

---
+++ pertemuan-16/index.js
@@ -0,0 +1,11 @@
+/* ------- Import/Export Module ------- */
+
+import {hello, world, name, number} from './hello.js'
+import default_function from './solo.js'
+//kurung kurawal tidak dibutuhkan jika ada default pada module
+
+hello ('John Doe')
+world()
+console.log(name)
+console.log(number)
+default_function()
