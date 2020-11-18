b37c306bdd16ed7b5fcc2879c06c8bd311fcf55b
bryanakaclay <js.bryann@gmail.com>
2020-11-19 00:51:26 +0800

    pertemuan-24
    ujicoba desain

--- pertemuan-21/src/Components/card.js
+++ pertemuan-21/src/Components/card.js
@@ -3,10 +3,11 @@ import React from 'react'
 const Card = ({name, username, email, phone}) =>{
     return (
         <div className="card-container">
-            <p>{name}</p>
-            <p>{username}</p>
-            <p>{email}</p>
-            <p>{phone}</p>
+            <h3>Employee Card</h3>
+            <p>~ Name : {name}</p>
+            <p>~ Username : {username}</p>
+            <p>~ Email : {email}</p>
+            <p>~ Phone : {phone}</p>
         </div>
     )
 }
